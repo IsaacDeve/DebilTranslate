9f4e115afdef63c73a05f2447c9057818532435a
IsaacDeve <156547171+IsaacDeve@users.noreply.github.com>
2024-08-05 14:15:46 -0700

    Add files via upload

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -32,9 +32,9 @@
             this.splitContainer2 = new System.Windows.Forms.SplitContainer();
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.richTextBox2 = new System.Windows.Forms.RichTextBox();
-            this.OutputLabel = new System.Windows.Forms.Label();
-            this.ToTranslate = new System.Windows.Forms.Label();
             this.trnbutton = new System.Windows.Forms.Button();
+            this.ToTranslate = new System.Windows.Forms.Label();
+            this.OutputLabel = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
             this.splitContainer1.Panel1.SuspendLayout();
             this.splitContainer1.Panel2.SuspendLayout();
@@ -64,8 +64,8 @@
             this.splitContainer1.Panel2.Controls.Add(this.ToTranslate);
             this.splitContainer1.Panel2.Controls.Add(this.OutputLabel);
             this.splitContainer1.Panel2.RightToLeft = System.Windows.Forms.RightToLeft.No;
-            this.splitContainer1.Size = new System.Drawing.Size(844, 371);
-            this.splitContainer1.SplitterDistance = 283;
+            this.splitContainer1.Size = new System.Drawing.Size(505, 282);
+            this.splitContainer1.SplitterDistance = 215;
             this.splitContainer1.TabIndex = 0;
             // 
             // splitContainer2
@@ -81,63 +81,73 @@
             // splitContainer2.Panel2
             // 
             this.splitContainer2.Panel2.Controls.Add(this.richTextBox2);
-            this.splitContainer2.Size = new System.Drawing.Size(844, 283);
-            this.splitContainer2.SplitterDistance = 430;
+            this.splitContainer2.Size = new System.Drawing.Size(505, 215);
+            this.splitContainer2.SplitterDistance = 257;
             this.splitContainer2.TabIndex = 0;
             // 
             // richTextBox1
             // 
+            this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox1.Font = new System.Drawing.Font("Yu Gothic", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.richTextBox1.Location = new System.Drawing.Point(0, 0);
             this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(430, 283);
+            this.richTextBox1.Size = new System.Drawing.Size(257, 215);
             this.richTextBox1.TabIndex = 0;
             this.richTextBox1.Text = "";
             // 
             // richTextBox2
             // 
+            this.richTextBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.richTextBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox2.Font = new System.Drawing.Font("Yu Gothic", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.richTextBox2.Location = new System.Drawing.Point(0, 0);
             this.richTextBox2.Name = "richTextBox2";
-            this.richTextBox2.Size = new System.Drawing.Size(410, 283);
+            this.richTextBox2.Size = new System.Drawing.Size(244, 215);
             this.richTextBox2.TabIndex = 0;
             this.richTextBox2.Text = "";
             // 
-            // OutputLabel
+            // trnbutton
             // 
-            this.OutputLabel.AutoSize = true;
-            this.OutputLabel.Dock = System.Windows.Forms.DockStyle.Right;
-            this.OutputLabel.Location = new System.Drawing.Point(805, 0);
-            this.OutputLabel.Name = "OutputLabel";
-            this.OutputLabel.Size = new System.Drawing.Size(39, 13);
-            this.OutputLabel.TabIndex = 1;
-            this.OutputLabel.Text = "Output";
+            this.trnbutton.BackColor = System.Drawing.Color.Cyan;
+            this.trnbutton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.trnbutton.Font = new System.Drawing.Font("Trebuchet MS", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.trnbutton.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.trnbutton.Location = new System.Drawing.Point(0, 16);
+            this.trnbutton.Name = "trnbutton";
+            this.trnbutton.Size = new System.Drawing.Size(92, 27);
+            this.trnbutton.TabIndex = 3;
+            this.trnbutton.Text = "TRANSLATE";
+            this.trnbutton.UseVisualStyleBackColor = false;
+            this.trnbutton.Click += new System.EventHandler(this.trnbutton_Click);
             // 
             // ToTranslate
             // 
             this.ToTranslate.AutoSize = true;
             this.ToTranslate.Dock = System.Windows.Forms.DockStyle.Left;
+            this.ToTranslate.Font = new System.Drawing.Font("Yu Gothic", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.ToTranslate.Location = new System.Drawing.Point(0, 0);
             this.ToTranslate.Name = "ToTranslate";
-            this.ToTranslate.Size = new System.Drawing.Size(83, 13);
+            this.ToTranslate.Size = new System.Drawing.Size(92, 14);
             this.ToTranslate.TabIndex = 2;
             this.ToTranslate.Text = "Text to translate";
             // 
-            // trnbutton
+            // OutputLabel
             // 
-            this.trnbutton.Location = new System.Drawing.Point(3, 16);
-            this.trnbutton.Name = "trnbutton";
-            this.trnbutton.Size = new System.Drawing.Size(74, 27);
-            this.trnbutton.TabIndex = 3;
-            this.trnbutton.Text = "Translate";
-            this.trnbutton.UseVisualStyleBackColor = true;
-            this.trnbutton.Click += new System.EventHandler(this.trnbutton_Click);
+            this.OutputLabel.AutoSize = true;
+            this.OutputLabel.Dock = System.Windows.Forms.DockStyle.Right;
+            this.OutputLabel.Font = new System.Drawing.Font("Yu Gothic", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.OutputLabel.Location = new System.Drawing.Point(461, 0);
+            this.OutputLabel.Name = "OutputLabel";
+            this.OutputLabel.Size = new System.Drawing.Size(44, 14);
+            this.OutputLabel.TabIndex = 1;
+            this.OutputLabel.Text = "Output";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(844, 371);
+            this.ClientSize = new System.Drawing.Size(505, 282);
             this.Controls.Add(this.splitContainer1);
             this.Name = "Form1";
             this.Text = "DebilTranslate";
--- Form1.cs
+++ Form1.cs
@@ -15,6 +15,7 @@ namespace DebilTranslate
         public Form1()
         {
             InitializeComponent();
+            StartTheme();
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -26,6 +27,25 @@ namespace DebilTranslate
         {
             // richTextBox2.Text = "";
             text = text.Replace(first, second);
+        }
+
+        void StartTheme()
+        {
+            Color back = ColorTranslator.FromHtml("#2e3242");
+            Color backText = ColorTranslator.FromHtml("#13151c");
+            Color textColor = ColorTranslator.FromHtml("#9398ab");
+
+            this.BackColor = back;
+            this.trnbutton.ForeColor = backText;
+
+            this.richTextBox1.BackColor = backText;
+            this.richTextBox2.BackColor = backText;
+
+            this.richTextBox1.ForeColor = textColor;
+            this.richTextBox2.ForeColor = textColor;
+
+            this.OutputLabel.ForeColor = textColor;
+            this.ToTranslate.ForeColor = textColor;
         }
 
         void Translate()
@@ -79,6 +99,42 @@ namespace DebilTranslate
             Tra(ref inputText, ',', 'б');
             Tra(ref inputText, '.', 'ю');
             Tra(ref inputText, '/', '.');
+
+            //caps
+
+            Tra(ref inputText, 'Q', 'Й');
+            Tra(ref inputText, 'W', 'Ц');
+            Tra(ref inputText, 'E', 'У');
+            Tra(ref inputText, 'R', 'К');
+            Tra(ref inputText, 'T', 'Е');
+            Tra(ref inputText, 'Y', 'Н');
+            Tra(ref inputText, 'U', 'Г');
+            Tra(ref inputText, 'I', 'Ш');
+            Tra(ref inputText, 'O', 'Щ');
+            Tra(ref inputText, 'P', 'З');
+            Tra(ref inputText, '{', 'Х');
+            Tra(ref inputText, '}', 'Ъ');
+            Tra(ref inputText, 'A', 'Ф');
+            Tra(ref inputText, 'S', 'Ы');
+            Tra(ref inputText, 'D', 'В');
+            Tra(ref inputText, 'F', 'А');
+            Tra(ref inputText, 'G', 'П');
+            Tra(ref inputText, 'H', 'Р');
+            Tra(ref inputText, 'J', 'О');
+            Tra(ref inputText, 'K', 'Л');
+            Tra(ref inputText, 'L', 'Д');
+            Tra(ref inputText, ':', 'Ж');
+            Tra(ref inputText, '"', 'Э');
+            Tra(ref inputText, 'Z', 'Я');
+            Tra(ref inputText, 'X', 'Ч');
+            Tra(ref inputText, 'C', 'С');
+            Tra(ref inputText, 'V', 'М');
+            Tra(ref inputText, 'B', 'И');
+            Tra(ref inputText, 'N', 'Т');
+            Tra(ref inputText, 'M', 'Ь');
+            Tra(ref inputText, '<', 'Б');
+            Tra(ref inputText, '>', 'Ю');
+            //Tra(ref inputText, '?', ',');
 
             richTextBox2.Text = inputText;
         }
